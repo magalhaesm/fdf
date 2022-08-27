ce64e40de6435603d114adfb2c5a9fe87af72144
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-08-27 17:41:26 -0300

    refactor: change event trigger

--- src/events.c
+++ src/events.c
@@ -6,41 +6,41 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 18:18:06 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/08/27 13:00:12 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2022/08/27 17:43:08 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fdf.h"
 
 static int	quit(t_data *data);
-static int	key_release(int keysym, t_data *data);
-int	zoom(int keysym, t_data *data);
+static int	key_press(int keysym, t_data *data);
+static int	zoom(int keysym, t_data *data);
 
 void	event_handler(t_data *data)
 {
-	mlx_hook(data->win_ptr, EnterNotify, EnterWindowMask, draw_scene, data);
+	mlx_expose_hook(data->win_ptr, draw_scene, data);
 	mlx_hook(data->win_ptr, DestroyNotify, NoEventMask, quit, data);
-	mlx_key_hook(data->win_ptr, key_release, data);
+	mlx_hook(data->win_ptr, KeyPress, KeyPressMask, key_press, data);
 }
 
 int	zoom(int keysym, t_data *data)
 {
 	if (keysym == XK_Up)
-		data->scene.scale += 2;
+		data->scene.scale += 1;
 	else
-		data->scene.scale -= 2;
+		data->scene.scale -= 1;
 	data->scene.scaled_col = (data->scene.cols * data->scene.scale) / 2;
 	data->scene.scaled_row = (data->scene.rows * data->scene.scale) / 2;
-	draw_scene(data);
 	return (EXIT_SUCCESS);
 }
 
-static int	key_release(int keysym, t_data *data)
+static int	key_press(int keysym, t_data *data)
 {
 	if (keysym == XK_Escape)
 		quit(data);
 	if (keysym == XK_Up || keysym == XK_Down)
 		zoom(keysym, data);
+	draw_scene(data);
 	return (EXIT_SUCCESS);
 }
 
